83c6c1c5d293e4f2e74e53cbd498518a99cc1485
Christian Buco <christianbuco17@gmail.com>
2017-03-15 09:08:15 -1000

    JSON NOOOOOOOOOO

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// var JsonDataBody = [
+// 	{
+// 		"header":["center","h1","h4"]
+// 	},
+// 	{
+// 		"div":{
+// 			"class":"container",
+
+// 		}
+// 	},
+// ];
+
+// var JsonDataStuff = [
+// 	{
+// 		"div": ["one","article"],
+// 		"a": "https://github.com/JustBacon",
+// 	}
+// ]
+
+var simplifiedJSON = [
+	{
+		"journalEntry":"1",
+		"date": "August 15,2016",
+		"title": "My First Entry",
+		"content": ["I took this class because I am interested in learning programming. I want to know how evrything works behind the scene. For example, I want to know how certain apps or games work.","What I want to get out of this class","To learn the basic of programming. Experience different programs and learn it."
+	},
+	{
+		"journalEntry":"2",
+		"date": "August 24,2016",
+		"title": "UX vs UI",
+		"content": ["Difference Between UX and UI", "User Interface(UI) When the User and system interact.","User Experience(UX) overall experience using a product"]
+
+	},
+	{
+		"journalEntry":"3",
+		"date"
+	}
+]
